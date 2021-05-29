6f0719a1dc4558e97a02157c16358c1e8a485c36
UniqueDevStorm <kirito7416@gmail.com>
2021-05-29 10:23:28 +0900

    feat: Discord button

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -14,13 +14,13 @@ export default function Navbar() {
                 </Link>
             </div>
             <div className='float-right'>
-                <Link href='https://discord.gg/CfXfrqSX'>
-                    <a href=''>
-                        <div>
-
-                        </div>
-                    </a>
-                </Link>
+                <div>
+                    <Link href='https://discord.gg/CfXfrqSX'>
+                        <a href='https://discord.gg/CfXfrqSX'>
+                            <h1 className='text-xl font-thin pr-10 pt-5 text-gray-400'>디스코드</h1>
+                        </a>
+                    </Link>
+                </div>
             </div>
         </nav>
     )
