aa1a944d7a8c0c91e955df955170f5d081f32ce2
UniqueDevStorm <kirito7416@gmail.com>
2021-05-30 17:08:40 +0900

    feat: Animation in discord button

--- components/Header.tsx
+++ components/Header.tsx
@@ -12,11 +12,11 @@ export default function Header() {
             </div>
             <div className="flex-grow"/>
             <div>
-                    <Link href='https://discord.gg/CfXfrqSX'>
-                        <a href='https://discord.gg/CfXfrqSX' target='_blank'>
-                            <h1 className='sm:text-xl text-lg font-thin text-gray-400'>디스코드</h1>
-                        </a>
-                    </Link>
+                <Link href='https://discord.gg/CfXfrqSX'>
+                    <a href='https://discord.gg/CfXfrqSX' target='_blank'>
+                        <h1 className='transition duration-500 hover:text-black sm:text-xl text-lg font-thin text-gray-400'>디스코드</h1>
+                    </a>
+                </Link>
             </div>
         </nav>
     )
--- pages/index.tsx
+++ pages/index.tsx
@@ -8,9 +8,9 @@ export default function Home() {
                 description='팀 Ariah 메인 페이지입니다.'
             />
             <div className='sm:pl-52 sm:pt-52'>
-                <h1 className='sm:text-6xl text-gray-400'>Team Ariah.</h1>
-                <h1 className='sm:text-2xl text-blue-800'>유저가 만드는 공간</h1>
-                <h1 className='sm:text-2xl text-green-400'>Team Ariah</h1>
+                <h1 className='sm:text-6xl text-gray-300'>Team Ariah.</h1>
+                <h1 className='sm:text-2xl text-blue-400'>유저가 만드는 공간</h1>
+                <h1 className='sm:text-2xl text-blue-400'>Team Ariah</h1>
             </div>
         </div>
     )
